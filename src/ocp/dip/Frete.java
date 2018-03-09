7a5cdf3ac4a6a4646ef355045fd201e6469997dc
Master <Master@DESKTOP-15A1TNH>
2018-03-09 08:35:24 -0300

    alterações TabelaPrecoPadrao e Frete

--- src/ocp/dip/Frete.java
+++ src/ocp/dip/Frete.java
@@ -2,6 +2,7 @@ package ocp.dip;
 
 public class Frete implements ServicoDeEntrega{
 
+	/* Classe de teste: A coesão poderia ser afetada já que a regra poderia mudar */
 	public double para(String cidade) {
 		if("SAO PAULO".equals(cidade.toUpperCase())){
 			return 15;
--- src/ocp/dip/TabelaDePrecoPadrao.java
+++ src/ocp/dip/TabelaDePrecoPadrao.java
@@ -2,6 +2,7 @@ package ocp.dip;
 
 public class TabelaDePrecoPadrao implements TabelaDePreco {
 
+	/* Classe de teste: A coesão poderia ser afetada já que a regra poderia mudar */
 	public double descontoPara(double valor) {
 		if(valor > 1000) return 0.05;
 		if(valor > 5000) return 0.03;
